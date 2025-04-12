51e5a11ee8fc5f062c79a1517547e275c4f1512d
fangarh <57655184+fangarh@users.noreply.github.com>
2025-04-12 14:33:42 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,14 +23,14 @@ export default {
                     same ++
                     continue;
                 }
-                percent++;
                
                 var dwgModel = await intercept.findIntersection(firstObjects[i], toIntersect[j], true);
 
                 ctx.setStatusBarMessage(`Обработано ${percent+1} из ${total}, ${Math.ceil(percent / total * 100.)}%.`+
                     ` Затрачено времени: ${Math.ceil((new Date().getTime() - startTime)/1000)}c.`+
                     ` Найдено пересечений: ${count}. Совпадений: ${same}  `);
-                
+                    
+                percent++;
                 if(dwgModel)
                     count ++;
             }
@@ -38,7 +38,9 @@ export default {
         const endTime = new Date().getTime();
         //var dwgModel = await intercept.findIntersection(result[0], result[1]) as DwgModel3d;
         // console.log(count);
-        ctx.setStatusBarMessage("Прогресс 100%", 0.5);
+        ctx.setStatusBarMessage(`Обработано ${percent} из ${total}, ${Math.ceil(percent / total * 100.)}%.`+
+                    ` Затрачено времени: ${Math.ceil((new Date().getTime() - startTime)/1000)}c.`+
+                    ` Найдено пересечений: ${count}. Совпадений: ${same}  `);
         ctx.showMessage(`Найдено пересечений: ${count} \nЗатрачено времени: ${Math.ceil((endTime - startTime)/1000)}c\n`+
                         `Совпадений: ${same} ` );
     }
